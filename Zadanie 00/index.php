a655b02c6fea2fc8ba18dea21bdfa451d25b8436
2ir02 <marcin.panter@wp.pl>
2023-05-19 10:26:18 +0200

    initial commit

---
+++ Zadanie 00/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Zadanie 00</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <h1>Witaj na mojej stronie</h1>
+    <hr>
+    <?php
+        echo "<b>Witaj świecie</b>";
+    ?>
+</body>
+</html>
